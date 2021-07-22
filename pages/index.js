467899e6f1b91d65b479b10b9d5cdf6c3e531fe8
fadhilsaheer <fadhilsaheer@gmail.com>
2021-07-22 11:41:44 +0530

    start a new document text

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,17 @@ export default function Home() {
         <title>Google Docs Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <Header />
+
+      <section className="bg-[#F8F9FA] pb-10 px-10">
+        <div className="max-w-3xl mx-auto">
+          <div className="py-6">
+            <h2 className="text-gray-700 text-lg">Start a new document</h2>
+          </div>
+        </div>
+      </section>
+
     </div>
   )
 }
